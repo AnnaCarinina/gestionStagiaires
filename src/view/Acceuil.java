8bfa506335aef0682e9d5c97712fdf38b65ffa10
anacarinina <leilaelkhrof@gmail.com>
2020-08-28 21:19:07 +0000

    commit par zahra

--- src/view/Acceuil.java
+++ src/view/Acceuil.java
@@ -24,7 +24,7 @@ public class Acceuil extends Application{
     private static Stage stage;
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("welcome.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Departement.fxml"));
         
         Scene scene = new Scene(root);
         stage.setScene(scene);
